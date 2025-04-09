00fe3e77e3e81b8957418bbbcf1e3168a9d53576
daniilyrkin <daniilyrkin@yandex.ru>
2025-04-09 19:20:03 +0300

    Auto commit Ср 09 апр 2025 19:20:03 MSK

--- mistralai_bot/handlers/admin.py
+++ mistralai_bot/handlers/admin.py
@@ -26,7 +26,7 @@ admin.message.filter(F.chat.id.in_({ADMIN}))
 async def admin_comands(message: Message):
     keyboard = await keyboards.reply_key_builder(
         ['/users', '/requests', '/logs', '/statistics month', '/statistics day', '/settings'])
-    await message.answer('Получи свои права админа11', reply_markup=keyboard)
+    await message.answer('Получи свои права админа12', reply_markup=keyboard)
 
 
 @admin.message(Command('users'))
