5f07426092242e58ad2958f814382b07372906e2
daniilyrkin <daniilyrkin@yandex.ru>
2025-04-09 17:00:18 +0300

    mini3

--- mistralai_bot/handlers/admin.py
+++ mistralai_bot/handlers/admin.py
@@ -26,7 +26,7 @@ admin.message.filter(F.chat.id.in_({ADMIN}))
 async def admin_comands(message: Message):
     keyboard = await keyboards.reply_key_builder(
         ['/users', '/requests', '/logs', '/statistics month', '/statistics day', '/settings'])
-    await message.answer('Админ', reply_markup=keyboard)
+    await message.answer('Получи панель', reply_markup=keyboard)
 
 
 @admin.message(Command('users'))
